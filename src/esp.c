40b761df4695dcd29c74722d509613f6b47ceb36
cuki2 <mauricio@krebs.com.br>
2017-02-15 08:39:44 -0200

    Signed-off-by: cuki2 <mauricio@krebs.com.br>

--- src/esp.c
+++ src/esp.c
@@ -7,13 +7,13 @@
 
 #include <18F25K22.h>
 
-#fuses INTRC, PLLEN, PRIMARY, NOIESO, NOPUT, NOBROWNOUT
+#fuses INTRC, NOPLLEN, PRIMARY, NOIESO, NOPUT, NOBROWNOUT
 #fuses NOWDT, NOPBADEN, NOHFOFST, MCLR, NOSTVREN, NOLVP
 #fuses NOXINST, NODEBUG, NOPROTECT, NOCPB, NOCPD, NOWRTC
 #fuses NOWRTB, NOWRTD, NOEBTR, NOEBTRB
 
-#use delay(internal=64MHz)
-#use rs232(uart1, baud=115200)
+#use delay(internal=16MHz)
+#use rs232(uart1, baud=9600)
 
 #define BUFF_S	1024
 
@@ -24,23 +24,6 @@
 uint8_t webPage[] =
 		"<head><meta http-equiv=\" \"refresh\" \" content=\" \"3\" \">\"</head><h1><u>ESP8266 - Web Server</u></h1><h2>Porta";
 bool esp_recived;
-uint8_t esp_buffer[BUFF_S];
-uint16_t esp_index;
-
-#INT_RDA
-void isr_rda() {
-	set_timer0(0);
-	setup_timer_0(T0_INTERNAL | T0_DIV_128);
-	esp_buffer[esp_index] = getc();
-	esp_index++;
-
-	if (esp_index >= BUFF_S)
-		esp_index = 0;
-
-	clear_interrupt(INT_RDA);
-
-	return;
-}
 
 #INT_TIMER0
 void isr_timer0() {
@@ -51,23 +34,11 @@ void isr_timer0() {
 	return;
 }
 
-void mcu_init(void) {
-	esp_index = 0;
-	esp_recived = false;
-	clear_interrupt(INT_RDA);
-	clear_interrupt(INT_TIMER0);
-	enable_interrupts(INT_RDA);
-	enable_interrupts(INT_TIMER0);
-	enable_interrupts(GLOBAL);
-
-	return;
-}
-
-uint8_t esp_find() {
+uint8_t esp_find(uint8_t *str) {
 	uint8_t ipd[] = "+IPD,";
 	uint8_t ret = 0xFF;
 
-	ret = strstr(esp_buffer, ipd);
+	ret = strstr(str, ipd);
 
 	return ret;
 }
@@ -83,7 +54,7 @@ void esp_webPage(bool input) {
 void esp_chipSend(uint8_t conId, uint16_t length) {
 	printf("AT+CIPSEND=");
 	printf("%u", conId);
-	printf(",");
+	printf(", ");
 	printf("%lu\n\r", length);
 
 	return;
@@ -106,18 +77,43 @@ void esp_init(void) {
 	return;
 }
 
+void mcu_init(void) {
+//	esp_index = 0;
+	esp_recived = false;
+//	clear_interrupt(INT_RDA);
+	clear_interrupt(INT_TIMER0);
+//	enable_interrupts(INT_RDA);
+	enable_interrupts(INT_TIMER0);
+	enable_interrupts(GLOBAL);
+
+	return;
+}
+
 int main(void) {
 	uint8_t ch_id;
+	uint8_t esp_buff[512];
+	uint16_t esp_index;
 
 	ch_id = 0xFF;
+	esp_index = 0;
 	esp_init();
 	delay_ms(1000);
 	mcu_init();
 
 	while (TRUE) {
+
+		if (kbhit()) {
+			esp_buff[esp_index++] = getc();
+			set_timer0(0);
+			setup_timer_0(T0_INTERNAL | T0_DIV_8);
+
+			if (esp_index >= 512)
+				esp_index = 0;
+		}
+
 		if (esp_recived) {
 			esp_recived = false;
-			ch_id = esp_find();
+			ch_id = esp_find(esp_buff);
 
 			if (ch_id != 0xFF) {
 				esp_chipSend(ch_id, sizeof(webPage) + 6);
